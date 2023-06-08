30bed4247d3365076ede064c9fe9d789ec07d515
robhyrk <rob.hyrkiel@gmail.com>
2023-06-08 18:48:31 -0400

    style: add custom breakpoints

--- app/components/header/navigation.tsx
+++ app/components/header/navigation.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Navigation() {
   return (
-    <header className="flex justify-between gap-8">
+    <header className="flex flex-col sm:flex-row justify-between gap-8">
       <Image className="flex-1 max-w-[324px]" src="/wsx-logo.svg" alt="Washington Stock Exchange logo" width={324} height={58} />
       <EmailSubscribe />
     </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
+      screens: {
+        sm: "375px",
+        md: "600px",
+        lg: "905px",
+        xl: "1240px",
+        "2xl": "1440px"
+      },
       colors: {
         red: {
           DEFAULT: "#AF2738"
