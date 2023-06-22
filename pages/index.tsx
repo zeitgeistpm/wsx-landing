38e07d040c48e4a669acf5b44048ea08bc1396f3
robhyrk <rob.hyrkiel@gmail.com>
2023-06-22 14:08:38 -0400

    chore: rearrange files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
+import React from "react"
+import EmailSubscribe from "@/app/components/email-subscribe"
 import Image from "next/image"
-import EmailSubscribe from "./components/email-subscribe"
 
-export default function Home() {
+const HomePage: React.FC = () => {
   return (
     <section className="relative overflow-hidden xl:max-w-screen-2xl xl:mx-auto">
       <Image className="py-10 right-[590px] md:max-w-none md:relative lg:static lg:-translate-x-0 lg:max-w-full mx-auto" src="/wsx-text.png" alt="Washington Stock Exchange logo" width={1658} height={600} quality={100} />
@@ -9,3 +10,5 @@ export default function Home() {
     </section>
   )
 }
+
+export default HomePage
