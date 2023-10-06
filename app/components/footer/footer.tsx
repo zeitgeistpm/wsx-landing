a0c752f027432365fa0146b8b9aa8b4b84a8fa11
robhyrk <rob.hyrkiel@gmail.com>
2023-10-06 00:04:20 -0400

    chore: remove footer desc

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -4,9 +4,9 @@ import { roboto } from "../../fonts/fonts"
 export default function Footer() {
   return (
     <footer className={`mt-auto flex flex-col items-center gap-8 justify-center max-w-[850px] mx-auto text-center px-6 text-sm ${roboto.className}`}>
-      <p className=" font-medium">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+      {/* <p className=" font-medium">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p> */}
       <Image className="" src="/powered-zeitgeist.svg" alt="Zeitgeist logo" width={277} height={35} />
-      <p className="text-gray-light">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+      {/* <p className="text-gray-light">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p> */}
     </footer>
   )
 }
